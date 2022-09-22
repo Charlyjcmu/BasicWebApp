f6b176fbe3614d587ea556b279b994ff153cf626
Charlyjcmu <60170577+Charlyjcmu@users.noreply.github.com>
2022-09-22 16:03:49 -0400

    Changing %20

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -14,6 +14,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("your name")) {
             return "jhcjrr";
         }
+        if (query.toLowerCase().contains("Eiffel")) {
+            return "Paris";
+        }
         if (query.toLowerCase().contains("plus")) {
             String queryWithSpace = query.replaceAll("%20","");
             String[] parts = queryWithSpace.split(" ");
@@ -21,6 +24,25 @@ public class QueryProcessor {
             int n2 = Integer.parseInt(parts[4]);
             int sum = n1+n2;
             return String.valueOf(sum);
+        }
+        if (query.toLowerCase().contains("multiplied")) {
+            String queryWithSpace = query.replaceAll("%20","");
+            String[] parts = queryWithSpace.split(" ");
+            int n1 = Integer.parseInt(parts[2]);
+            int n2 = Integer.parseInt(parts[5]);
+            int product = n1*n2;
+            return String.valueOf(product);
+        }
+        if (query.toLowerCase().contains("minus")) {
+            String queryWithSpace = query.replaceAll("%20","");
+            String[] parts = queryWithSpace.split(" ");
+            int n1 = Integer.parseInt(parts[2]);
+            int n2 = Integer.parseInt(parts[4]);
+            int min = n1-n2;
+            return String.valueOf(min);
+        }
+        if (query.toLowerCase().contains("banana")) {
+            return "yellow";
         }
         return "";
     }
